4dfa2e8508e27fdb7b43dd0f48ff25dbd6c5f9fa
anharfathoni <anharaf28@gmail.com>
2018-12-10 08:37:51 +0700

    server

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  res.status(200).send({ message: 'Welcome Bro!' })
+  res.status(200).json({ message: 'Welcome Bro!' })
 })
 
 module.exports = router
